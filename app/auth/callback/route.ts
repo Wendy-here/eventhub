53cf72b8b8dc30c347d8f5a8cb5f6bd043643755
Wendy-here <uyen.nguyen@gradion.com>
2026-05-08 09:06:08 +0000

    fix types

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -16,7 +16,7 @@ process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 cookies:{
 getAll(){return cookieStore.getAll()},
 setAll(cookiesToSet:any){
-cookiesToSet.forEach(({name,value,options})=>{
+cookiesToSet.forEach(({name,value,options}:any)=>{
 cookieStore.set(name,value,options)
 })
 }
