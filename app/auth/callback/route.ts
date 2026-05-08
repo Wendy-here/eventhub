405d62fa2b00bf1e9a16eb67c46ff867c121f0a7
Wendy-here <uyen.nguyen@gradion.com>
2026-05-08 08:54:31 +0000

    fix: type error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,7 +15,7 @@ process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 {
 cookies:{
 getAll(){return cookieStore.getAll()},
-setAll(cookiesToSet){
+setAll(cookiesToSet:any){
 cookiesToSet.forEach(({name,value,options})=>{
 cookieStore.set(name,value,options)
 })
